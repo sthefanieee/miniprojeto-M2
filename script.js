98e320480f1fb4d82ab5b4aac1137ff047fcb7bb
sthefanie <sthefaniesantana1306@gmail.com>
2023-10-17 23:50:31 -0300

    concluindo

--- script.js
+++ script.js
@@ -1,40 +1,91 @@
 class Note {
-    constructor(text) {
-      this.text = text;
-    }
+  constructor(text) {
+    this.text = text;
   }
-
-var notesArray = [];
-
-function saveNote() {
-  // Salva a anotação
-  var noteText = document.getElementById("myDiary").value;
-  notesArray.push(noteText);
-  localStorage.setItem("notes", JSON.stringify(notesArray));
 }
 
+
+
+notesArray = [];
+
+const tabela = document.getElementById("table")
+
+function saveNote() {
+// Salva a anotação
+let noteText = new Note(document.getElementById('myDiary').value);
+
+if(noteText.text.length > 0){
+  notesArray.push(noteText)  
+  alert('item salvo !');
+  console.log(notesArray)
+  displayNotes()
+}else{
+  alert('nada para salvar !')
+}
+
+
+}
+
+function conteudo(note,index){
+      return `  <table class="table">
+      <tr>
+          <th>Salvos <span>"${index}"</span></th>
+      <tr>
+          <td >${note.text}</td>
+      </tr>
+      </tr>
+      </table>
+`
+}
+
+
+
 function editNote() {
-  // Edita a anotação
-  var noteIndex = prompt("Digite a anotação que você deseja editar:");
+// Edita a anotação
+
+if(notesArray && notesArray.length > 0){
+  var noteIndex = prompt("Digite o numero da anotação que você quer editar (exemplo: 0 = primeira, 1 = segunda, 2 = terceira ...)");
   var newText = prompt("Digite o novo segredo:");
-  notesArray[noteIndex] = newText;
-  localStorage.setItem("notes", JSON.stringify(notesArray));
+  if(noteIndex && newText){
+    notesArray[noteIndex].text = newText;
+    displayNotes()
+  }else{
+    alert('nenhuma mudança feita!')
+  }
+
+}else{
+  alert('nenhuma nota salva!')
+}
+
 }
 
 function deleteNote() {
-  // Exclui a anotação
-  var noteIndex = prompt("Digite a anotação que você deseja excluir:");
+// Exclui a anotação
+if(notesArray && notesArray.length > 0){
+  
+  var noteIndex = prompt("Digite o numero da anotação que você quer excluir (exemplo: 0 = primeira, 1 = segunda, 2 = terceira ...)");
+  if(noteIndex){
   notesArray.splice(noteIndex, 1);
-  localStorage.setItem("notes", JSON.stringify(notesArray));
+  displayNotes()
+}else{
+  alert('Nenhuma alteração feita!')
+}
+}else{
+  alert('Nenhuma nota salva!')
+}
+
 }
 
 function displayNotes() {
-    // Exibe as anotações
-    var notesDiv = document.getElementById("notes");
-    notesDiv.innerHTML = ""; // Limpa as anotações antigas
-    for (var i = 0; i < notesArray.length; i++) {
-      var para = document.createElement("P");
-      para.innerText = "Anotação " + (i+1) + ": " + notesArray[i];
-      notesDiv.appendChild(para);
-    }
-  }
+  tabela.innerHTML = "";
+ 
+ 
+
+  notesArray.forEach((note,index) => {
+
+    tabela.innerHTML += conteudo(note,index)
+  })
+  
+
+
+}
